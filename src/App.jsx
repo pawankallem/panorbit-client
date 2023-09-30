f63929a517711176b3ce1a3c2f485ca95d2d096f
pawankallem <kallem.pavan1997@gmail.com>
2023-09-30 14:47:30 +0530

    logs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { Routers } from "./components/Routes";
-import { BrowserRouter } from "react-router-dom";
+// import { Routers } from "./components/Routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./components/pages/LandingPage";
+import { HomePage } from "./components/pages/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Provider store={store}>
-        </Provider> */}
-        <Routers />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { LandingPage } from "../pages/LandingPage";
 
